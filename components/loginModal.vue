a6e0f874ed1468bfad0008f1667f0a1cfd7f207b
Oliverbansk <banshikun@banshikuns-MacBook-Pro.local>
2024-01-19 00:42:20 +0800

    try of login

---
+++ components/loginModal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="modal">
+    <form @submit.prevent="login">
+      <input v-model="username" placeholder="Username" required>
+      <input v-model="password" type="password" placeholder="Password" required>
+      <button type="submit">登录</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      this.$emit('login-success', this.username);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  background-color: #fcfeff;
+  box-shadow: 0 0 10px rgba(117, 238, 199, 0.3);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,53 @@
 <template>
   <div>
-    <p>Some default layout content shared across all pages</p>
+    <p>Some default layout content shared across all pages (这是default layout中的内容，包括下面的身份显示与登录按钮)</p>
     <div>
-      <p>Nav</p>
-      <p><router-link to="/">Go to Home</router-link></p>
-      <p><router-link to="/hello">Go to Hello</router-link></p>
-      <p><router-link to="/uinfo/uinfo0">Go to Uinfo</router-link></p>
+      <nav>
+      <div v-if="!userLoggedIn">
+        <p>您当前身份为：游客</p>
+        <button @click="showLoginModal">点击登录</button>
+      </div>
+      <div v-else>
+          <p>您好，{{ username }}</p>
+        <button @click="logout">注销</button>
+      </div>
+    </nav>
+
+    <loginModal v-if="showModal" @login-success="handleLoginSuccess" />
     </div>
-    <slot />
   </div>
 </template>
+
+<script>
+import loginModal from '~/components/loginModal.vue';
+
+export default {
+  data() {
+    return {
+      userLoggedIn: false,
+      showModal: false,
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    showLoginModal() {
+      this.showModal = true;
+    },
+    handleLoginSuccess(username) {
+      this.showModal = false;
+      this.userLoggedIn = true;
+      this.username = username;
+      this.password = password;
+      // 加载数据、刷新页面
+    },
+    logout() {
+      this.userLoggedIn = false;
+      this.username = '';
+    },
+  },
+};
+</script>
+
+<style>
+</style>
